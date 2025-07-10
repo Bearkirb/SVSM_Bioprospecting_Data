ae3a303c1ccf562599f5b2b3409ab06092d08cfe
Bearkirb <163359154+Bearkirb@users.noreply.github.com>
2025-07-10 09:23:13 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,146 @@
+var dataJSON=`
+{
+  "Field_Samples": {
+    "sectionDescription": "Samples were collected from various potential sources of yeast in nature.  They are sorted by index and date.",
+    "data": [
+      {
+
+        "name": "BK1_6-30",
+        "sampleDescription": "Sampled from the surface of a ripening bluberry with a swab.  Located in the App State Living Learning Center, Boone NC",
+        "sampledBy": "Benny Kirby"
+      },
+      {
+        "name": "BK2_6-30",
+        "sampleDescription": "Sampled from the inside of an orange Stella D'oro flower in a planter beside the App State Library parking structure, Boone NC ",
+        "sampledBy": "Benny Kirby"
+      },
+      {
+        "name": "BK3_6-30",
+        "sampleDescription": "Sampled from the petals of a red flower (Monarda) in front of the App State Old Belk Library, Boone NC",
+        "sampledBy": "Benny Kirby"
+      },
+      {
+        "name": "BK4_7-1",
+        "sampleDescription": "From the inside of an Azalea flower on the North Summit of the Elk knob summit trail, Boone NC",
+        "sampledBy": "Benny Kirby"
+      },
+      {
+        "name": "BK5_7-1",
+        "sampleDescription": "Swabbed from the surface of a bug related growth on an Azalea branch, located on the North Summit of the Elk knob summit trail, Boone NC",
+        "sampledBy": "Benny Kirby"
+      },
+      {
+        "name": "BK6_7-1",
+        "sampleDescription": "Swabbed from the surface of an unripe blackberry, located on the North Summit of the Elk knob summit trail, Boone NC",
+        "sampledBy": "Benny Kirby"
+      },
+      {
+        "name": "BK7_7-2",
+        "sampleDescription": "Two whole Japanese Beetles in a sample tube, from a cherry tree mear the Ashe County Disk Golf Park parking lot",
+        "sampledBy": "Benny Kirby"
+      },
+      {
+        "name": "BK8_7-2",
+        "sampleDescription": "Swbbed from cracks in the bark of a Poplar tree in the Ashe County Disk Golf Park ",
+        "sampledBy": "Benny Kirby"
+      },
+      {
+        "name": "BK9_7-3",
+        "sampleDescription": "A whole ripening blackberry in a sample tube, from the Ted Mack trail, Boone NC",
+        "sampledBy": "Benny Kirby"
+      },
+      {
+        "name": "BK10_7-3",
+        "sampleDescription": "A bunch of pine needles including a portion of branch in a sample tube, from the Ted Mack trail, Boone NC",
+        "sampledBy": "Benny Kirby"
+      },
+      {
+        "name": "BK11_7-7",
+        "sampleDescription": "Swabbed from the surface of a ripening Pawpaw in a Pawpaw orchard, Boone NC",
+        "sampledBy": "Benny Kirby"
+      },
+      {
+        "name": "BK12_7-7",
+        "sampleDescription": "Swabbed from a black spot on the surface of a ripening Pawpaw in a Pawpaw orchard, Boone NC",
+        "sampledBy": "Benny Kirby"
+      },
+      {
+        "name": "BK13_7-7",
+        "sampleDescription": "Swabbed from cracks in the bark of a Pawpaw tree in a Pawpaw orchard, Boone NC",
+        "sampledBy": "Benny Kirby"
+      }
+    ]
+  },
+  "Isolated_Yeast": {
+    "sectionDescription": "Yeast monocultures were isolated into tubes with YPD solution.  Samples from the same plate are sorted alphabetically by culture size.",
+    "data": [
+      {
+        "name": "BK1A1",
+        "parentPlate":"BK1_6-30",
+        "containsYeast": true,
+        "containsBacteria": true
+      },
+      {
+        "name": "BK1B1",
+        "parentPlate":"BK1_6-30",
+        "containsYeast": false,
+        "containsBacteria": true
+      },
+      {
+        "name": "BK4A1",
+        "parentPlate":"BK4_7-1",
+        "containsYeast": true,
+        "containsBacteria": true
+      },
+      {
+        "name": "BK4B1",
+        "parentPlate":"BK4_7-1",
+        "containsYeast": true,
+        "containsBacteria": false
+      },
+      {
+        "name": "BK6A1",
+        "parentPlate":"BK6_7-1",
+        "containsYeast": false,
+        "containsBacteria": true
+      },
+      {
+        "name": "BK9A1",
+        "parentPlate":"BK9_7-3",
+        "containsYeast": true,
+        "containsBacteria": true
+      },
+      {
+        "name": "BK10A1",
+        "parentPlate":"BK10_7-3",
+        "containsYeast": false,
+        "containsBacteria": true
+      },
+      {
+        "name": "BK1A2",
+        "parentPlate":"BK4_7-1",
+        "containsYeast": true,
+        "containsBacteria": false
+      },
+      {
+        "name": "BK4A2",
+        "parentPlate":"BK6_7-1",
+        "containsYeast": false,
+        "containsBacteria": true
+      },
+      {
+        "name": "BK4B2",
+        "parentPlate":"BK9_7-3",
+        "containsYeast": true,
+        "containsBacteria": true
+      },
+      {
+        "name": "BK9A2",
+        "parentPlate":"BK10_7-3",
+        "containsYeast": false,
+        "containsBacteria": true
+      },
+    ]
+  }
+}
+`;
